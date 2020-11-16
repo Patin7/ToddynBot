d06b4fba714b982f748599f17121c460d8075868
Patin7 <71416943+Patin7@users.noreply.github.com>
2020-11-16 14:07:16 -0300

    Create base.js

---
+++ comandos/teste/base.js
@@ -0,0 +1,19 @@
+const Discord = require("discord")
+
+module.exports = {
+name: "base",
+run: async (bot, message) => {
+ message.channel.send("msg "+ message.author)//para mandar uma msg normal!
+ const embed = new Discord.MessageEmbed()
+ .setColor("cor em hex")
+ .setAuthor("titulo acima do .setTitle ;-;")
+ .setTitle("titulo da embed")
+ .setDescription("descrição da embed")
+ .addField("field da embed", "valor embed")
+ .setTimestamp()
+ .setFooter("footer da embed")
+ .setThumbnail("img da thumbnail")
+ 
+ message.channel.send(embed)
+ }
+}
